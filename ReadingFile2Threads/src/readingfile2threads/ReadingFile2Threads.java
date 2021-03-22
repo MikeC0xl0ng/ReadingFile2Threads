2b1ffc4495377883f9965d47c0268d880a40b360
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-03-22 22:25:41 +0100

    Playing around with output

--- ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
+++ ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
@@ -8,19 +8,27 @@ public class ReadingFile2Threads {
 
     public static void main(String[] args) {
         
-        FileReader highPriorityThread = new FileReader(Thread.NORM_PRIORITY + 3);
-        FileReader lowPriorityThread = new FileReader(Thread.NORM_PRIORITY - 3);
+        FileReader highPriorityThread = new FileReader(Thread.NORM_PRIORITY + 4);
+        FileReader lowPriorityThread = new FileReader(Thread.NORM_PRIORITY - 4);
         highPriorityThread.start();
         lowPriorityThread.start();
         
         try{
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         }catch(Exception ex){}
         
         lowPriorityThread.stop();
         highPriorityThread.stop();
-        System.out.println("High priority Thread's record is " + highPriorityThread.recordOfReadFiles);
-        System.out.println("Low priority Thread's record is " + lowPriorityThread.recordOfReadFiles);
+        int highPriorityThreadRecord = highPriorityThread.recordOfReadFiles;
+        int lowPriorityThreadRecord = lowPriorityThread.recordOfReadFiles;
+        System.out.println("High priority Thread's record is " + highPriorityThreadRecord);
+        System.out.println("Low priority Thread's record is  " + lowPriorityThreadRecord);
+        
+        int diff = highPriorityThreadRecord - lowPriorityThreadRecord;
+        
+        System.out.println("High priority Thread read " + diff + " more than low priority Thread, which corresponds to " + (diff / (lowPriorityThreadRecord * 100)) + "%");
+        
+        System.exit(0);
     }
     
 }
