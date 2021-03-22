7b7f1584b75a4e04077f5cf0772d17827c61128e
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-03-22 22:06:15 +0100

    Managing of the main

--- ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
+++ ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
@@ -7,8 +7,20 @@ package readingfile2threads;
 public class ReadingFile2Threads {
 
     public static void main(String[] args) {
-
-
+        
+        FileReader highPriorityThread = new FileReader(Thread.NORM_PRIORITY + 3);
+        FileReader lowPriorityThread = new FileReader(Thread.NORM_PRIORITY - 3);
+        highPriorityThread.start();
+        lowPriorityThread.start();
+        
+        try{
+            Thread.sleep(5000);
+        }catch(Exception ex){}
+        
+        lowPriorityThread.stop();
+        highPriorityThread.stop();
+        System.out.println("High priority Thread's record is " + highPriorityThread.recordOfReadFiles);
+        System.out.println("Low priority Thread's record is " + lowPriorityThread.recordOfReadFiles);
     }
     
 }
