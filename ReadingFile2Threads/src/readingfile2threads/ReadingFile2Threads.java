394fd7bf800688de5c63209ce59916f973d68eb4
Nick <62834545+MikeC0xl0ng@users.noreply.github.com>
2021-03-22 23:06:47 +0100

    Update

--- ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
+++ ReadingFile2Threads/src/readingfile2threads/ReadingFile2Threads.java
@@ -5,7 +5,7 @@ package readingfile2threads;
  * @author Nick
  */
 public class ReadingFile2Threads {
-
+    
     public static void main(String[] args) {
         
         FileReader highPriorityThread = new FileReader(Thread.NORM_PRIORITY + 4);
@@ -24,10 +24,10 @@ public class ReadingFile2Threads {
         System.out.println("High priority Thread's record is " + highPriorityThreadRecord);
         System.out.println("Low priority Thread's record is  " + lowPriorityThreadRecord);
         
-        int diff = highPriorityThreadRecord - lowPriorityThreadRecord;
-        int boh = diff/lowPriorityThreadRecord;
+        double diff = highPriorityThreadRecord - lowPriorityThreadRecord;
+        double boh = diff/lowPriorityThreadRecord;
         double percentageDiff = boh*100;
-        System.out.println("High priority Thread read " + diff + " more than low priority Thread, which corresponds to " + percentageDiff + "%");
+        System.out.println("High priority Thread read " + String.format("%.0f",diff) + " more than low priority Thread, which corresponds to " + String.format("%.2f",percentageDiff) + "%");
         
         System.exit(0);
     }
